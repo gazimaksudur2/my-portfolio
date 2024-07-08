d80e05ed6c5d2f7c69184aa09b4513daa4b34530
GaziMaksudur <gazimaksudur2@gmail.com>
2024-07-08 10:20:29 +0600

    Education section design completed

--- src/pages/Home/Education.jsx
+++ src/pages/Home/Education.jsx
@@ -1,3 +1,5 @@
+import { HiMiniFire } from "react-icons/hi2";
+
 const Education = () => {
     return (
         <div className='py-10 w-[70%] mx-auto'>
@@ -8,25 +10,32 @@ const Education = () => {
                 <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
                     <li>
                         <div className="timeline-middle">
-                            <svg
+                            <HiMiniFire className="h-5 w-5 text-purple-500 p-[1px] border-[2px] border-purple-500 rounded-full"/>
+                            {/* <img className="h-5 w-5 " src="https://www.svgrepo.com/show/500378/fire.svg" alt="running state" /> */}
+                            {/* <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 20 20"
                                 fill="currentColor"
-                                className="h-5 w-5">
+                                className="">
                                 <path
                                     fillRule="evenodd"
                                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
                                     clipRule="evenodd" />
-                            </svg>
+                            </svg> */}
                         </div>
-                        <div className="timeline-start mb-10 md:text-end">
-                            <time className="font-mono italic">1984</time>
-                            <div className="text-lg font-black">First Macintosh computer</div>
-                            The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh
+                        <div className="timeline-start mr-2 mb-10 md:text-end">
+                            <time className="font-mono italic">2022 - present</time>
+                            <h2 className="text-lg font-black">Computer Science and Engineering</h2>
+                            <h4 className="text-[#151515bd]">BSc. in Engineering</h4>
+                            {/* The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh
                             personal computer. It played a pivotal role in establishing desktop publishing as a general
                             office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed
                             in a beige case with integrated carrying handle; it came with a keyboard and single-button
-                            mouse.
+                            mouse. */}
+                            <div className="mt-2 flex justify-center items-center gap-3">
+                                <img className="w-8 h-8" src="https://www.sust.edu/images/logo.png" alt="sust logo" />
+                                <h2 className="font-medium text-[#151515db]">Shahjalal University of Science and Technology</h2>
+                            </div>
                         </div>
                         <hr />
                     </li>
@@ -37,7 +46,7 @@ const Education = () => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 20 20"
                                 fill="currentColor"
-                                className="h-5 w-5">
+                                className="h-5 w-5 text-purple-500">
                                 <path
                                     fillRule="evenodd"
                                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -45,11 +54,18 @@ const Education = () => {
                             </svg>
                         </div>
                         <div className="timeline-end mb-10">
-                            <time className="font-mono italic">1998</time>
-                            <div className="text-lg font-black">iMac</div>
-                            iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has
-                            been the primary part of Apple's consumer desktop offerings since its debut in August 1998,
-                            and has evolved through seven distinct forms
+                        <time className="font-mono italic">2022 - present</time>
+                            <h2 className="text-lg font-black">Computer Science and Engineering</h2>
+                            <h4 className="text-[#151515bd]">BSc. in Engineering</h4>
+                            {/* The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh
+                            personal computer. It played a pivotal role in establishing desktop publishing as a general
+                            office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed
+                            in a beige case with integrated carrying handle; it came with a keyboard and single-button
+                            mouse. */}
+                            <div className="mt-2 flex justify-center items-center gap-3">
+                                <img className="w-8 h-8" src="https://www.sust.edu/images/logo.png" alt="sust logo" />
+                                <h2 className="font-medium text-[#151515db]">Shahjalal University of Science and Technology</h2>
+                            </div>
                         </div>
                         <hr />
                     </li>
@@ -60,7 +76,7 @@ const Education = () => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 20 20"
                                 fill="currentColor"
-                                className="h-5 w-5">
+                                className="h-5 w-5 text-purple-500">
                                 <path
                                     fillRule="evenodd"
                                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -68,13 +84,18 @@ const Education = () => {
                             </svg>
                         </div>
                         <div className="timeline-start mb-10 md:text-end">
-                            <time className="font-mono italic">2001</time>
-                            <div className="text-lg font-black">iPod</div>
-                            The iPod is a discontinued series of portable media players and multi-purpose mobile devices
-                            designed and marketed by Apple Inc. The first version was released on October 23, 2001, about
-                            8+1⁄2 months after the Macintosh version of iTunes was released. Apple sold an estimated 450
-                            million iPod products as of 2022. Apple discontinued the iPod product line on May 10, 2022. At
-                            over 20 years, the iPod brand is the oldest to be discontinued by Apple
+                        <time className="font-mono italic">2022 - present</time>
+                            <h2 className="text-lg font-black">Computer Science and Engineering</h2>
+                            <h4 className="text-[#151515bd]">BSc. in Engineering</h4>
+                            {/* The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh
+                            personal computer. It played a pivotal role in establishing desktop publishing as a general
+                            office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed
+                            in a beige case with integrated carrying handle; it came with a keyboard and single-button
+                            mouse. */}
+                            <div className="mt-2 flex justify-center items-center gap-3">
+                                <img className="w-8 h-8" src="https://www.sust.edu/images/logo.png" alt="sust logo" />
+                                <h2 className="font-medium text-[#151515db]">Shahjalal University of Science and Technology</h2>
+                            </div>
                         </div>
                         <hr />
                     </li>
@@ -85,7 +106,7 @@ const Education = () => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 20 20"
                                 fill="currentColor"
-                                className="h-5 w-5">
+                                className="h-5 w-5 text-purple-500">
                                 <path
                                     fillRule="evenodd"
                                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -93,36 +114,18 @@ const Education = () => {
                             </svg>
                         </div>
                         <div className="timeline-end mb-10">
-                            <time className="font-mono italic">2007</time>
-                            <div className="text-lg font-black">iPhone</div>
-                            iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile
-                            operating system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on
-                            January 9, 2007. Since then, Apple has annually released new iPhone models and iOS updates. As
-                            of November 1, 2018, more than 2.2 billion iPhones had been sold. As of 2022, the iPhone
-                            accounts for 15.6% of global smartphone market share
-                        </div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-start mb-10 md:text-end">
-                            <time className="font-mono italic">2015</time>
-                            <div className="text-lg font-black">Apple Watch</div>
-                            The Apple Watch is a line of smartwatches produced by Apple Inc. It incorporates fitness
-                            tracking, health-oriented capabilities, and wireless telecommunication, and integrates with
-                            iOS and other Apple products and services
+                        <time className="font-mono italic">2022 - present</time>
+                            <h2 className="text-lg font-black">Computer Science and Engineering</h2>
+                            <h4 className="text-[#151515bd]">BSc. in Engineering</h4>
+                            {/* The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh
+                            personal computer. It played a pivotal role in establishing desktop publishing as a general
+                            office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed
+                            in a beige case with integrated carrying handle; it came with a keyboard and single-button
+                            mouse. */}
+                            <div className="mt-2 flex justify-center items-center gap-3">
+                                <img className="w-8 h-8" src="https://www.sust.edu/images/logo.png" alt="sust logo" />
+                                <h2 className="font-medium text-[#151515db]">Shahjalal University of Science and Technology</h2>
+                            </div>
                         </div>
                     </li>
                 </ul>
--- src/pages/Home/Experience.jsx
+++ src/pages/Home/Experience.jsx
@@ -3,7 +3,33 @@ const Experience = () => {
         <div className='py-10 w-[70%] mx-auto'>
             <div className='space-y-2 flex flex-col justify-center items-center text-center'>
                 <h1 className='bg-clip-text text-transparent bg-gradient-to-r from-purple-800 to-gray-400 text-2xl font-bold'>Experience</h1>
-                <p className="text-sm text-[#151515bd] pt-5">{"I'm Gazi Maksudur Rahman, a third-year Computer Science student at SUST, where I've built a robust foundation in programming, algorithms, data structures, and databases. I've applied this knowledge in numerous projects, focusing on web development technologies such as HTML, CSS, Tailwind CSS, React, Node.js, Express.js, MongoDB, and MySQL. My proficiency in competitive programming has strengthened my problem-solving skills and coding efficiency. Delve into my projects to explore how I leverage these skills to create dynamic and responsive web applications."}</p>
+            </div>
+            <div className="mt-8">
+                <ol className="relative border-s border-gray-500">
+                    <li className="mb-10 ms-4">
+                        <div className="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -start-1.5 border border-white"></div>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-400">February 2022</time>
+                        <h3 className="text-lg font-semibold text-gray-900">Application UI code in Tailwind CSS</h3>
+                        <p className="mb-4 text-base font-normal text-gray-500">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+                        <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700">Learn more <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg></a>
+                    </li>
+                    <li className="mb-10 ms-4">
+                        <div className="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -start-1.5 border border-white"></div>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-400">March 2022</time>
+                        <h3 className="text-lg font-semibold text-gray-900">Marketing UI design in Figma</h3>
+                        <p className="text-base font-normal text-gray-500">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+                    </li>
+                    <li className="ms-4">
+                        <div className="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -start-1.5 border border-white"></div>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-400">April 2022</time>
+                        <h3 className="text-lg font-semibold text-gray-900">E-Commerce UI code in Tailwind CSS</h3>
+                        <p className="text-base font-normal text-gray-500">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+                    </li>
+                </ol>
+
+
             </div>
         </div>
     );
